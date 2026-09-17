007f930c90f7de0a427b75b4954c6cf6053730cf
MuhaddisSaleem <132219519+MuhaddisSaleem@users.noreply.github.com>
2026-09-17 16:11:38 +0500

    feat: render restored header and hero above booking flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { BookingComponent } from './booking/booking.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BookingComponent],
-  template: '<app-booking></app-booking>'
+  imports: [HeroComponent, BookingComponent],
+  template: `
+    <app-hero></app-hero>
+    <app-booking></app-booking>
+  `
 })
 export class AppComponent {}
